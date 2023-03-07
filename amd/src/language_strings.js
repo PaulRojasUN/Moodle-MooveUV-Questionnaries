7ae2d581d32e4fdff016b0224a01a1e9db38448a
PaulRojasUN <paul.rojas@correounivalle.edu.co>
2023-03-07 11:46:57 -0500

    Corrected bug: Login page keeps reloading

---
+++ amd/src/language_strings.js
@@ -0,0 +1,33 @@
+/*
+ * @module      theme_mooveuv/modal_login
+ * @author      2022 Iader E. Garcia Gomez <iadergg@gmail.com>
+ * @copyright   2022 Área de Nuevas Tecnologías - DINTEV - Universidad del Valle <desarrollo.ant@correounivalle.edu.co>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import $ from 'jquery';
+
+export const init = () => {
+
+    var urlLocation = window.location.pathname;
+    var urlSplit = urlLocation.split("/");
+    var locationSearch = window.location.search;
+
+    var arrayQuestionnaires = ["1761690"];
+
+    var parameter = locationSearch.split("=")[1];
+
+    if (urlSplit.includes('questionnaire') && urlSplit.includes('view.php')) {
+
+        if (arrayQuestionnaires.includes(parameter)) {
+            $('.complete > a').html("Participe del proceso de votación");
+        }
+    } else if (urlSplit.includes('questionnaire') && (urlSplit.includes('complete.php') || urlSplit.includes('preview.php'))) {
+        if (arrayQuestionnaires.includes(parameter)) {
+            $('input[name=submit]').val("Votar");
+            $('.floatprinticon').hide();
+            $('.mod_questionnaire_completepage generalbox>h3').html('Gracias por participar en el proceso de votación.');
+            $('.mod_questionnaire_previewpage>h2').html('Previsualizando');
+        }
+    }
+};
